c559d48f933dee65c98d036f68a3ec2358cd42db
André Silva <2493377+askpt@users.noreply.github.com>
2022-05-27 11:53:09 +0100

    Added Json transcoding.

--- Speedy.Host/Program.cs
+++ Speedy.Host/Program.cs
@@ -7,14 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
     // Setup a HTTP/2 endpoint without TLS.
-    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http1AndHttp2);
 });
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
