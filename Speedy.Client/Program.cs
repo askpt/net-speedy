09c9e2e81ae8e4f5a784f2553a79485317f33b44
André Silva <2493377+askpt@users.noreply.github.com>
2022-05-27 12:26:39 +0100

    Added http call.

--- Speedy.Client/Program.cs
+++ Speedy.Client/Program.cs
@@ -1,18 +1,50 @@
-﻿using Grpc.Net.Client;
+﻿using System.Diagnostics;
+using Grpc.Net.Client;
 using Speedy.Host;
 
-AppContext.SetSwitch(
-    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-var httpHandler = new HttpClientHandler();
-// Return `true` to allow certificates that are untrusted/invalid
-httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions { HttpHandler = httpHandler });
-var client = new Greeter.GreeterClient(channel);
+const string Name = "Speedy";
 
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
+await DoGrpcCall(Name);
+await DoGrpcHttpCall(Name);
 
 Console.WriteLine("Shutting down");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
+
+async Task DoGrpcCall(string name)
+{
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    var httpHandler = new HttpClientHandler();
+    // Return `true` to allow certificates that are untrusted/invalid
+    httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+    var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions { HttpHandler = httpHandler });
+    var client = new Greeter.GreeterClient(channel);
+
+    var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+    Console.WriteLine("Greeting: " + reply.Message);
+
+    stopWatch.Stop();
+    var time = stopWatch.ElapsedMilliseconds;
+    Console.WriteLine($"Operation DoGrpcCall took {time}ms");
+}
+
+async Task DoGrpcHttpCall(string name)
+{
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    // Do a Http Call
+    var httpClient = new HttpClient();
+    var response = await httpClient.GetAsync($"http://localhost:5001/v1/greeter/{name}");
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+
+    stopWatch.Stop();
+    var time = stopWatch.ElapsedMilliseconds;
+    Console.WriteLine($"Operation DoGrpcHttpCall took {time}ms");
+}
