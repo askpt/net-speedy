0d8c8b3f09c28337d308a05521ae146653897775
André Silva <2493377+askpt@users.noreply.github.com>
2022-07-08 10:14:59 +0100

    Added some more k6 tests.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 import grpc from 'k6/net/grpc';
+import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 const client = new grpc.Client();
 client.load(['Protos'], 'greet.proto');
 
 export default () => {
+    doGrpcTest();
+    doHttpTest();
+    sleep(1);
+};
+
+function doGrpcTest() {
     client.connect('localhost:5000', {
         plaintext: true
     });
@@ -16,8 +23,17 @@ export default () => {
         'status is OK': (r) => r && r.status === grpc.StatusOK,
     });
 
-    console.log(JSON.stringify(response.message));
+    // console.log(JSON.stringify(response.message));
 
     client.close();
-    sleep(1);
-};
+}
+
+function doHttpTest() {
+    const response = http.get('http://localhost:5001/v1/greeter/Bert');
+
+    check(response, {
+        'status is 200': (r) => r && r.status === 200
+    });
+
+    // console.log(JSON.stringify(response.body));
+}
